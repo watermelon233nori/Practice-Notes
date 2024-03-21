98b8aaeaa97073b3233a8d6680e4e83b08147021
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2024-03-21 11:39:40 +0800

    modified:   leetcode/3/lengthOfLongestSubstring.c

--- leetcode/3/lengthOfLongestSubstring.c
+++ leetcode/3/lengthOfLongestSubstring.c
@@ -3,13 +3,18 @@
 
 int lengthOfLongestSubstring(char *s)
 {
-    int tmp = 0, length = 0, *ptr=0;
+    int tmp = 0, length = 0, head = 0;
+    for (int a=0;s[a];a++){
+        if(tmp==0&&head==0){
+            
+        }
+    }
 }
 
 int main()
 {
     char s[] = "pwwkew";
     int length = lengthOfLongestSubstring(s);
-    printf("String: %s\nExpected Output: 3\nResult: %d",s,length);
+    printf("String: %s\nExpected Output: 3\nResult: %d", s, length);
     return 0;
 }
