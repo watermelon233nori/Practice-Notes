6ac0344a81ad187627f62778574e9b959bcfff62
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2024-03-27 22:22:04 +0800

    new file:   leetcode/66/plusOne.c

---
+++ leetcode/66/plusOne.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+int *plusOne(int *digits, int digitsSize, int *returnSize)
+{
+    int *tmp = (int *)malloc((digitsSize + 1) * sizeof(int));
+    memset(tmp, NULL, sizeof(tmp));
+}
+
+int main()
+{
+    int digits[5];
+    int digitsSize = sizeof(digits) / sizeof(digits[0]);
+    int returnSize;
+    int *result = plusOne(digits, digitsSize, &returnSize);
+    printf("Input Array: [4, 3, 2, 1]\nExpected Output: [4, 3, 2, 2]\nResult: [");
+    for (int i = 0; i < returnSize; i++)
+    {
+        printf("%d",result[i]);
+        if (i!=returnSize-1)
+        {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+    return 0;
+}
