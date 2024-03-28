eb19b963b3c56aa48bdb62c4b5c86484b39a667e
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2024-03-28 11:39:57 +0800

    modified:   leetcode/66/plusOne.c

--- leetcode/66/plusOne.c
+++ leetcode/66/plusOne.c
@@ -12,19 +12,39 @@ int *plusOne(int *digits, int digitsSize, int *returnSize)
     }
     for (int i = digitsSize; i > 0; i--)
     {
-        /* code */
+        if (tmp[i] + 1 == 10)
+        {
+            tmp[i] = 0;
+            tmp[i - 1]++;
+        }
     }
-    
+    *returnSize = tmp[0]==0 ? digitsSize : digitsSize + 1;
+    int *arr = (int *)malloc(*returnSize * sizeof(int));
+    if (*returnSize == digitsSize)
+    {
+        for (int i = 0; i < *returnSize; i++)
+        {
+            arr[i] = tmp[i + 1];
+        }
+    }
+    else
+    {
+        for (int i = 0; i < *returnSize; i++)
+        {
+            arr[i] = tmp[i];
+        }
+    }
     free(tmp);
+    return arr;
 }
 
 int main()
 {
-    int digits[5];
+    int digits[5] = {9, 9};
     int digitsSize = sizeof(digits) / sizeof(digits[0]);
     int returnSize;
     int *result = plusOne(digits, digitsSize, &returnSize);
-    printf("Input Array: [4, 3, 2, 1]\nExpected Output: [4, 3, 2, 2]\nResult: [");
+    printf("Input Array: [9, 9]\nExpected Output: [1, 0, 0]\nResult: [");
     for (int i = 0; i < returnSize; i++)
     {
         printf("%d", result[i]);
