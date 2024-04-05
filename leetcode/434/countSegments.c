2f7115e10807ec8c5546e6533f14b8eb1b19f8cc
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2024-04-06 01:29:48 +0800

    modified:   leetcode/434/countSegments.c

--- leetcode/434/countSegments.c
+++ leetcode/434/countSegments.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
-int countSegments(char * s){
-    
+int countSegments(char *s)
+{
+    int ans = 0;
+    for (char *i = s; *i; i++)
+    {
+        
+    }
+    return ans;
+}
+
+int main()
+{
+    char str[] = "";
 }
