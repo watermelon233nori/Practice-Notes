684a0cbd8e0283b97ff7ac1de3a34f98a3beb853
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2024-04-17 20:40:37 +0800

    new file:   leetcode/2824/countPairs.c

---
+++ leetcode/2824/countPairs.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int countPairs(int *nums, int numsSize, int target)
+{
+    int *left = nums, *right = nums;
+    if (numsSize==1)
+    {
+        return 0;
+    }
+    else
+    {
+        right++;
+    }
+    while (right - left + 1 == numsSize)
+    {
+    }
+}
+
+int main()
+{
+
+    return 0;
+}
