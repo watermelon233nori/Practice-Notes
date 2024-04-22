d6abc8b1aea79d49ad39c93fa32b28e280252116
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2024-04-22 17:29:55 +0800

    new file:   leetcode/3019/countKeyChanges.c

---
+++ leetcode/3019/countKeyChanges.c
@@ -0,0 +1,30 @@
+int countKeyChanges(char *s)
+{
+    int ans = 0;
+    char *l = s;
+    if (s[0] == '\0')
+    {
+        return 0;
+    }
+    char *r = s + 1;
+    while (*r)
+    {
+        if (*l == *r || *l + 32 == *r || *l - 32 == *r)
+        {
+        }
+        else
+        {
+            l = r;
+            ans++;
+        }
+        r++;
+    }
+    return ans;
+}
+
+int main()
+{
+    char *s = "aAbBcC";
+    printf("%d\n", countKeyChanges(s));
+    return 0;
+}
