39e558992a54a128b7573557b3541d5bbaede779
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2024-05-09 17:25:54 +0800

    Add No. 2079 on LeetCode Problem Set.

---
+++ leetcode/2079/wateringPlants.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int wateringPlants(int *plants, int plantsSize, int capacity)
+{
+    int tank = capacity, steps = 1;
+    for (int pos = 0; pos == plantsSize;)
+    {
+        if (tank < plants[pos])
+        {
+            tank = capacity;
+            steps += pos * 2 + 1;
+        }
+        else
+        {
+            tank -= plants[pos];
+            plants[pos] = 0;
+            pos++, steps++;
+        }
+    }
+    return steps;
+}
+
+int main()
+{
+    int plants[] = {2, 2, 3, 3};
+    int capacity = 5;
+    printf("%d", wateringPlants(plants, 4, capacity));
+    return 0;
+}
