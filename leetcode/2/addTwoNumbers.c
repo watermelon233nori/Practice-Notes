aab9bfe544e54d52bdb9a7db850daf7a4472714c
watermelon233nori <92632254+watermelon233nori@users.noreply.github.com>
2024-05-20 15:45:09 +0800

    Modify temporarily.

--- leetcode/2/addTwoNumbers.c
+++ leetcode/2/addTwoNumbers.c
@@ -6,11 +6,12 @@ struct ListNode
 {
     int val;
     struct ListNode *next;
-};
+}ListNode, *LinkList;
 
 struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2)
 {
     struct ListNode *ans = (struct ListNode *)malloc(sizeof(struct ListNode));
+    struct ListNode *ptr = ans;
     for (int carry = 0; l1;)
     {
         if (l1->val + l2->val >= 10)
@@ -18,9 +19,12 @@ struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2)
             ans->val = l1->val + l2->val - 10;
         }
         l1 = l1->next, l2 = l2->next;
-        ans->next = (struct ListNode *)malloc(sizeof(struct ListNode));
-        ans = ans->next;
+        ptr->next = (struct ListNode *)malloc(sizeof(struct ListNode));
+        ptr = ptr->next;
     }
+    ptr = NULL;
+    free(ptr);
+    return ans;
 }
 
 int main(int argc, char const *argv[])
