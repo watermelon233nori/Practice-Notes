1517706047c0483ae9562636605bb92906e2db0e
watermelon233nori <92632254+watermelon233nori@users.noreply.github.com>
2024-05-31 14:26:58 +0800

    [LeetCode No.2] Check the head of the l1 and l2 linklist. [README] Change a little.

--- leetcode/2/addTwoNumbers.c
+++ leetcode/2/addTwoNumbers.c
@@ -12,6 +12,8 @@ struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2)
 {
     struct ListNode *ans = (struct ListNode *)malloc(sizeof(struct ListNode));
     struct ListNode *ptr = ans;
+    l1 = l1->val ? l1 : l1->next;
+    l2 = l2->val ? l2 : l2->next;
     for (int carry = 0; l1;)
     {
         if (l1->val + l2->val >= 10)
@@ -29,5 +31,6 @@ struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2)
 
 int main(int argc, char const *argv[])
 {
+    struct LinkList *l1;
     return 0;
 }
