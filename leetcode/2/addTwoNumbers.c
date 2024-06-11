7f13174bb4b7c9fa28e69686f3d5d0dbbefb8646
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2024-06-11 15:31:04 +0800

    Refactor the addTwoNumbers() function on No.2 on LeetCode Problem Set.

--- leetcode/2/addTwoNumbers.c
+++ leetcode/2/addTwoNumbers.c
@@ -6,25 +6,39 @@ struct ListNode
 {
     int val;
     struct ListNode *next;
-}ListNode, *LinkList;
+} ListNode, *LinkList;
 
 struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2)
 {
-    struct ListNode *ans = (struct ListNode *)malloc(sizeof(struct ListNode));
+    struct ListNode *ans = (struct LinkNode *)malloc(sizeof(struct ListNode));
+    ans->next = NULL, ans->val = 0;
     struct ListNode *ptr = ans;
-    for (int carry = 0; l1;)
+    int carry = 0;
+    if (!l1->val)
     {
-        if (l1->val + l2->val >= 10)
-        {
-            ans->val = l1->val + l2->val - 10;
-        }
-        l1 = l1->next, l2 = l2->next;
-        ptr->next = (struct ListNode *)malloc(sizeof(struct ListNode));
-        ptr = ptr->next;
+        l1 = l1->next;
     }
-    ptr = NULL;
-    free(ptr);
-    return ans;
+    if (!l2->val)
+    {
+        l2 = l2->next;
+    }
+
+    while (l1 || l2)
+    {
+        if (l1->val + l2->val > 9) {
+            carry = 1;
+            ans = l1->val + l2->val - 10;
+        }
+        else {
+            carry = 0;
+            ans = l1->val + l2->val;
+        }
+        l1 = l1->next ? l1->next : l1;
+        l2 = l2->next ? l2->next : l2;
+        ans->next = l1 || l2 ? (struct ListNode *)malloc(sizeof(struct ListNode)) : NULL;
+    }
+
+    return ans->next;
 }
 
 int main(int argc, char const *argv[])
