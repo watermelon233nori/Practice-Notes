a9620e0c96ee04ee8aa0396c45aac3093837f817
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2024-07-25 10:16:43 +0000

    Finish No. 1154 on LeetCode Problem Set.

--- leetcode/1154/dayOfYear.c
+++ leetcode/1154/dayOfYear.c
@@ -1,33 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int stringToInt(char *src)
-{
-    int length = 0;
-    for (char *ptr, ptr = src; *ptr <= '9' && *ptr >= '0'; length++, ptr++)
-        ;
-    if (length == 4)
-    {
-        return (int)(src[0] - '0') * 1000 + (int)(src[1] - '0') * 100 + (int)(src[2] - '0') * 10 + (int)(src[3] - '0');
-    }
-    if (length == 2)
-    {
-        return (int)(src[0] - '0') * 10 + (int)(src[1] - '0');
-    }
-    return 0;
-}
-
 int dayOfMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 int dayOfYear(char *date)
 {
-    int ans = 0;
-    for (int *i = dayOfMonth; i < dayOfMonth + stringToInt(date + 6); i++)
+    date[4] = '\0';
+    date[7] = '\0';
+    int ans = 0, year = atoi(date), month = atoi(date + 5) - 1, day = atoi(date + 8);
+    for (int *i = dayOfMonth; i < dayOfMonth + month; i++)
     {
         ans += *i;
     }
-    ans += stringToInt(date + 9);
-    return (stringToInt(date) % 4 == 0 && stringToInt(date) % 100 != 0) ? ans + 1 : ans;
+    ans += day;
+    if (month > 1 && (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)))
+    {
+        ++ans;
+    }
+    return ans;
 }
 
 int main()
