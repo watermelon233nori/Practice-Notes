7b6a0ba42a438434374510a993c243451f9357ee
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2024-08-21 10:12:17 +0000

    Remove unnecessary comments in main program templates

--- templates/main/main.c
+++ templates/main/main.c
@@ -1,17 +1,14 @@
+#include <math.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include <stdio.h>
-// #include <stdlib.h>
-// #include <stdbool.h>
-// #include <string.h>
-// #include <math.h>
 
 int main() {
-    // Read the number of test cases.
     int t;
     scanf("%d", &t);
-
     for (int i = 0; i < t; ++i) {
         // Solution
-        /* Code block */
     }
     return 0;
 }
--- templates/main/main.cpp
+++ templates/main/main.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 #include <algorithm>
+#include <iostream>
+
+using namespace std;
 
 int main() {
-    /* Input & output optimization refers to https://oi-wiki.org/contest/io/ */
-    // Disable the synchronization with C standard input
-    std::ios::sync_with_stdio(false);
-    // Disable the tie between std::cin and std::cout
-    std::cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
     return 0;
 }
