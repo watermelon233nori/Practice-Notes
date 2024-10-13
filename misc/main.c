12ce9d77cbc1ab7da935bf2c5de9847f5488384b
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2024-10-14 03:41:23 +0800

    Omit.

--- misc/main.c
+++ misc/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <ctype.h>
 #include <stdint.h>
 #include <limits.h>
@@ -8,7 +9,35 @@
 #include <math.h>
 
 int main() {
-    printf("%d", );
+    int n, m;
+    scanf("%d%d", &n, &m);
+    int arr[n];
+    memset(arr, 0, sizeof(int) * n);
+    for (int i = 0; i < m; ++i) {
+        int in;
+        scanf("%d", &in);
+        in--;
+        arr[in] = 1;
+    }
+    int headptr = 0, tailptr = n - 1;
+    int headDistance, tailDistance, tailM = 0, headM = 0;
+    bool pig = true, pigStatus = true;
+    for (int i = 0; i < n; ++i, pig = !pig) {
+        bool direction = true; // true = right, false = left
+        int flag = 0;
+        for (int i = headptr; flag != 2 && i < n; ++i) {
+            if (arr[i]) {
+                flag = 1;
+            }
+            if (flag == 1 && arr[i] == 0) {
+                flag = 2;
+            }
+        }
+    }
+    if (pigStatus) {
+        printf("Xiaozhu Hahaha\n");
+    } else {
+        printf("Goldye\n");
+    }
     return 0;
-    
 }
