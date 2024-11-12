efdbd2d6f22764f784253f203072aab50de59067
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2024-11-13 02:24:31 +0800

    Omit.

---
+++ misc/oi-wiki/basic/enumerate/example.cpp
@@ -0,0 +1,15 @@
+#include <cstring>
+#include <iostream>
+#define IOS std::ios::sync_with_stdio(false); std::cin.tie(nullptr);
+constexpr int MAXN = 100000;
+
+int solve(int n, int a[]) {
+    
+}
+
+int main() {
+    IOS;
+    int n;
+    std::cin >> n;
+    
+}
