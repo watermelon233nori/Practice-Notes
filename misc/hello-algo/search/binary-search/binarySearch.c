df54afa6154a21ee5aa1868edb335f7ebfc7ed86
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2024-11-15 17:37:58 +0800

    Add binary search on the notes for Hello-Algo.

---
+++ misc/hello-algo/search/binary-search/binarySearch.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int binarySearch(int nums[], int len, int val) {
+    int i = 0, j = len - 1;
+    while (i <= j) {
+        int m = i + (i + j) / 2;
+        // code
+    }
+}
+
+int main() {
+    int nums[] = { 1,3,6,8,12,15,23,26,31,35 };
+    return 0;
+}
