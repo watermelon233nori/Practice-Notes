b6a4c42c548f1794b757838a3fd2d4cfd2ccb009
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2024-11-19 02:57:35 +0800

    Omit.

--- misc/hello-algo/search/binary-search/binarySearch.c
+++ misc/hello-algo/search/binary-search/binarySearch.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int binarySearch(int nums[], int len, int val) {
+/**
+ * @param nums 输入数组应为升序数组
+ */
+int binarySearch(int nums[], int len, int target) {
     int i = 0, j = len - 1;
     while (i <= j) {
         int m = i + (i + j) / 2;
-        // code
+        if (nums[m] == target) {
+            return m;
+        } else if (nums[m] < target) {
+            i = m + 1;
+        } else {
+            j = m - 1;
+        }
     }
+    return -1; // Fail
 }
 
 int main() {
     int nums[] = { 1,3,6,8,12,15,23,26,31,35 };
+    printf("9's index: %d\n", binarySearch(nums, 10, 9));
+    printf("6's index: %d\n", binarySearch(nums, 10, 6));
     return 0;
 }
