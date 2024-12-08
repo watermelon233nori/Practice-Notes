c2ed31ddcc15f1e77b95cdb7c0951217a429f896
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2024-12-08 22:49:07 +0800

    PTA PAT-Basic #1002 AC. PTA PAT-Basic #1003 Add.

--- pta/PAT-Basic/1002/1865610199875489792.cpp
+++ pta/PAT-Basic/1002/1865610199875489792.cpp
@@ -4,7 +4,7 @@
 #pragma GCC optimize(2)
 #define IOS std::ios::sync_with_stdio(false);std::cin.tie(nullptr);
 using namespace std;
-string pinyin[] = { "ling","yi","er","san","si","wu","liu","qi","ba","jiu"};
+string pinyin[] = { "ling","yi","er","san","si","wu","liu","qi","ba","jiu" };
 
 int main() {
     IOS;
@@ -15,9 +15,9 @@ int main() {
         ans += (ch - '0');
     }
     str = to_string(ans);
-    for (auto ch: str) {
-        cout << pinyin[ch-'0'] << ' ';
+    for (auto ch = str.begin(); ch != str.end() - 1; ch++) {
+        cout << pinyin[*ch - '0'] << ' ';
     }
-    cout << '\n';
+    cout << pinyin[*(str.end()-1) - '0'] << '\n';
     return 0;
 }
---
+++ pta/PAT-Basic/1003/a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#pragma GCC optimize(2)
+#define IOS std::ios::sync_with_stdio(false);std::cin.tie(nullptr);
+using namespace std;
+
+int main() {
+    IOS;
+    
+    return 0;
+}
