67da262539cd9d5701651a037bf71f0356a66a48
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2025-03-14 17:08:03 +0800

    gcc diagnostic test

---
+++ misc/cpp/string/a.cpp
@@ -0,0 +1,16 @@
+#pragma GCC diagnostic push
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    vector<int> a = {3,5,6,7};
+    vector<int> b = {7,4,9,2,6};
+    for (auto i: a) {
+        cout << i << " ";
+    }
+    return 0;
+}
+
+#pragma GCC diagnostic pop
