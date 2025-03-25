83aca33df7aa7af7db62d1bb4ff1bd0b4c9d2339
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2025-03-25 16:10:31 +0800

    Leetcode added 3 problems, renamed 1 corresponding folder of 1 problem

---
+++ workspace/leetcode/difference-of-number-of-distinct-values-on-diagonals/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> differenceOfDistinctValues(vector<vector<int>>& grid) {
+        auto m = grid.size();
+        auto n = grid.at(0).size();
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}
