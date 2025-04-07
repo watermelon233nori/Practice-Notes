f1bc5b676a2495ba682e67c3c6f17e4599ba6842
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2025-04-07 14:49:49 +0800

    Leetcode #257 AC

--- workspace/leetcode/binary-tree-paths/619728626.cpp
+++ workspace/leetcode/binary-tree-paths/619728626.cpp
@@ -22,28 +22,30 @@ public:
     vector<string> binaryTreePaths(TreeNode* root) {
         TreeNode* current = root;
         char meetRoot = 0;
-        std::function<void(TreeNode&)> recurTail = [this, &recurTail](TreeNode& node) {
-            if ((this->isLeaf(node))) {
-                string item;
-                #define RIGHT "->"
-                for (auto i = path.begin(); i != path.end() - 1; ++i) {
-                    item += (to_string(*i) + RIGHT);
-                }
-                item += to_string(node.val);
-                ret.emplace_back(item);
-                if (!path.empty()) path.pop_back();
-            } else {
-                path.emplace_back(node.val);
-                if (node.left) {
-                    recurTail(*node.left);
-                }
-                if (node.right) {
-                    recurTail(*node.right);
-                }
-            }
-        };
         recurTail(*current);
         return move(ret);
+    }
+
+    void recurTail(TreeNode& node) {
+        if (isLeaf(node)) {
+            string item;
+            #define RIGHT "->"
+            for (auto i = path.begin(); i != path.end(); ++i) {
+                item += (to_string(*i) + RIGHT);
+            }
+            item += to_string(node.val);
+            ret.emplace_back(item);
+            
+        } else {
+            path.emplace_back(node.val);
+            if (node.left) {
+                recurTail(*node.left);
+            }
+            if (node.right) {
+                recurTail(*node.right);
+            }
+            if (!path.empty()) path.pop_back();
+        }
     }
 
     inline bool isLeaf(TreeNode& node) {
@@ -55,6 +57,7 @@ int main() {
     // testcase 1
     Solution loc;
     TreeNode nodes[4];
+    TreeNode single(1);
     nodes[0].val = 1;
     nodes[1].val = 2;
     nodes[2].val = 3;
@@ -64,6 +67,19 @@ int main() {
     nodes[1].right = &(nodes[3]);
     auto ret = loc.binaryTreePaths(&(nodes[0]));
     for (auto i: ret) {
+        cout << i << '\n';
+    }
+    auto singleRet = loc.binaryTreePaths(&single);
+    cout << single.val << '\n';
+
+    TreeNode nodes2[3];
+    nodes2[0].val = 1;
+    nodes2[1].val = 2;
+    nodes2[2].val = 3;
+    nodes2[0].left = &(nodes2[1]);
+    nodes2[0].right = &(nodes2[2]);
+    auto ret2 = loc.binaryTreePaths(&(nodes2[0]));
+    for (auto i: ret2) {
         cout << i << '\n';
     }
     return 0;
