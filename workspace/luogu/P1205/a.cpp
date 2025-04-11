77ba53981331b56f60fa40665a5cb8877f1aba7c
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2025-04-11 18:37:45 +0800

    Luogu P1205 Add

--- workspace/luogu/P1205/a.cpp
+++ workspace/luogu/P1205/a.cpp
@@ -2,19 +2,18 @@
 
 using namespace std;
 
-#define Reflect do { \
-    /* code */ \
-} while (false);
-#define pmsg(x) do{if(!cmpChange){cout<<#x<<'\n';return 0;}}while(false)
+// Print if operated matrix matches target
+#define pmsg(x) do{if(!cmpChange){cout<<#x<<'\n';return 0;}cmpChange=false;}while(false)
 
 char square[10][10];
 char target[10][10];
+char reflect[10][10];
 int n;
 
 inline bool cmpImage(char value[10][10]) {
     for (int i = 0; i < n; ++i) {
         auto cmp = memcmp(value[i], target[i], n);
-        if (!cmp) return true;
+        if (cmp) return true;
     }
     return false;
 }
@@ -33,28 +32,50 @@ int main() {
         cin >> target[i];
     }
 
-    // 6 Remain
-    bool cmpChange = cmpImage(square);
-    pmsg(6);
+    bool cmpChange = false;
 
     // 1 Rotate90
     for (int i = 0; i < n; ++i) {
-        bool diff = false;
-        for (int j = 0; j < n; ++j) { if (square[i][j] != target[j][n - i - 1]) { diff = true; break; } }
-        if (diff) { cmpChange = false; break; }
+        bool diff = false; for (int j = 0; j < n; ++j) { if (square[i][j] != target[j][n - i - 1]) { diff = true; break; } } if (diff) { cmpChange = true; break; }
     }
     pmsg(1);
 
     // 2 Rotate180
     for (int i = 0; i < n; ++i) {
-        bool diff = false;
-        for (int j = 0; j < n; ++i) { if (square[i][j] != target[n - i - 1][n - j - 1]) { diff = true; break; } }
-        if (diff) { cmpChange = false; break; }
+        bool diff = false; for (int j = 0; j < n; ++j) { if (square[i][j] != target[n - i - 1][n - j - 1]) { diff = true; break; } } if (diff) { cmpChange = true; break; }
     }
     pmsg(2);
 
     // 3 Rotate270
-    // todo
+    for (int i = 0; i < n; ++i) {
+        bool diff = false; for (int j = 0; j < n; ++j) { if (square[i][j] != target[n - j - 1][i]) { diff = true; break; } } if (diff) { cmpChange = true; break; }
+    }
+    pmsg(3);
+
+    // 4 Reflect
+    for (int i = 0; i < n; ++i) {
+        memcpy(reflect[i], square[n-i-1], n);
+    }
+    cmpChange = cmpImage(reflect);
+    pmsg(4);
+
+    // 5 GroupIt
+    for (int i = 0; i < n; ++i) { // 90
+        bool diff = false; for (int j = 0; j < n; ++j) { if (reflect[i][j] != target[j][n - i - 1]) { diff = true; break; } } if (diff) { cmpChange = true; break; }
+    }
+    pmsg(5);
+    for (int i = 0; i < n; ++i) { // 180
+        bool diff = false; for (int j = 0; j < n; ++j) { if (reflect[i][j] != target[n - i - 1][n - j - 1]) { diff = true; break; } } if (diff) { cmpChange = true; break; }
+    }
+    pmsg(5);
+    for (int i = 0; i < n; ++i) { // 270
+        bool diff = false; for (int j = 0; j < n; ++j) { if (reflect[i][j] != target[n - j - 1][i]) { diff = true; break; } } if (diff) { cmpChange = true; break; }
+    }
+    pmsg(5);
+
+    // 6 Ramain
+    cmpChange = cmpImage(square);
+    pmsg(6);
 
     // 7 Unavailable
     cout << "7\n";
