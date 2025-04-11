1e3c09dceb14a413d37a3086139ee0f159d328c0
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2025-04-11 19:41:00 +0800

    add 2 fast macros

---
+++ templates/macro/macro1.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+
+#define fastio() std::ios::sync_with_stdio(false);std::cin.tie(nullptr)
+#define yesno(cond) std::cout<<((cond)?"YES\n":"NO\n")
+#define MOD 1000000007
+#define gcd(a,b) std::__gcd(a,b)
+#define lcm(a,b) (a/gcd(a,b)*b)
