afb28e9bf5a6eeefd0b672d0fff37e5647f0d40b
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2025-04-13 16:45:46 +0800

    Luogu Contest #203994 Add.

---
+++ workspace/luogu/contests/203994/A/a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define el cout<<'\n';
+unsigned short nums[(int)1e6];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cin >> nums[i];
+    }
+    for (int i = n - 1; i >= 1; --i) {
+        
+    }
+    el;
+    return 0;
+}
