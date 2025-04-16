9b6324712c196def0565eb1865ace3d8c1182cc5
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2025-04-17 00:55:38 +0800

    Luogu #P1321 Add.

---
+++ workspace/luogu/P1321/a.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char boy[] = "yob", girl[] = "lrig";
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string str;
+    str.reserve(300);
+    cin >> str;
+    return 0;
+}
