05902d61ebe5b0bbdd4fb11f429de8aa0fa3c53c
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2025-04-17 15:47:17 +0800

    Update Luogu #P1321.

--- workspace/luogu/P1321/a.cpp
+++ workspace/luogu/P1321/a.cpp
@@ -2,7 +2,8 @@
 
 using namespace std;
 
-char boy[] = "yob", girl[] = "lrig";
+string boy = "boy";
+string girl = "girl";
 
 int main() {
     ios::sync_with_stdio(false);
@@ -10,5 +11,8 @@ int main() {
     string str;
     str.reserve(300);
     cin >> str;
+    for (const auto& i: str) {
+        
+    }
     return 0;
 }
