fe768304057f0643e6e81f3be79b7c6a79bfa753
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2025-04-18 02:56:10 +0800

    Luogu #P1321 Update, #P5735 Add.

--- workspace/luogu/P1321/a.cpp
+++ workspace/luogu/P1321/a.cpp
@@ -11,8 +11,15 @@ int main() {
     string str;
     str.reserve(300);
     cin >> str;
-    for (const auto& i: str) {
-        
+    for (const auto& i : str) {
+        if (i == '.') {
+            continue;
+        }
+        if (i == 'b') {
+            auto it = str.begin() + (&i - &str[0]);
+            auto misit = mismatch(it, str.end(), boy.begin()+1).first;
+            
+        }
     }
     return 0;
 }
