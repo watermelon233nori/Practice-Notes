10e41990999d3ad766c2d44ac82c99b3c36d5fb4
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2025-04-25 10:35:52 +0800

    Fix back the submission code for Codeforces #2106C.

--- workspace/codeforces/main/2106C/317081010.cpp
+++ workspace/codeforces/main/2106C/317081010.cpp
@@ -2,67 +2,38 @@
 
 using namespace std;
 using ll = long long;
-ll a[(size_t)2e5 + 10];
-ll b[(size_t)2e5 + 10];
+ll a[(size_t)2e5];
+ll b[(size_t)2e5];
 
 int main(int argc, char const* argv[]) {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t;
-    cin >> t;
+    int t;cin >> t;
     while (t--) {
-        int n, k;
-        cin >> n >> k;
-        ll amax = -1;
+        int n, k;cin >> n >> k;
+        ll amax = -1, amin = numeric_limits<ll>().max();
         for (int i = 0; i < n; ++i) {
-            cin >> a[i];
-            if (a[i] > amax) amax = a[i];
+            cin >> a[i]; amax = max(amax, a[i]); amin = min(amin, a[i]);
         }
-        ll sum = -1;
-        bool valid = true;
+        int sum = -1;
+        bool fullybroken = true;
         for (int i = 0; i < n; ++i) {
-            cin >> b[i];
-            if (b[i] != -1) {
-                ll current_sum = a[i] + b[i];
-                if (sum == -1) {
-                    sum = current_sum;
-                } else {
-                    if (current_sum != sum) {
-                        valid = false;
-                    }
-                }
-            }
-        }
-        if (!valid) {
-            cout << "0\n";
-            continue;
+            cin >> b[i]; if (fullybroken && b[i] != -1) { sum = a[i] + b[i]; fullybroken = false; }
         }
         if (sum != -1) {
-            ll min_a_plus_k = a[0] + k;
-            for (int i = 1; i < n; ++i) {
-                if (a[i] + k < min_a_plus_k) {
-                    min_a_plus_k = a[i] + k;
-                }
-            }
-            if (sum < amax || sum > min_a_plus_k) {
-                cout << "0\n";
+            if (sum - amin >= k || sum - amax < 0) {
+                cout << 0;
             } else {
-                cout << "1\n";
+                cout << 1;
             }
         } else {
-            ll max_a = amax;
-            ll min_a_plus_k = a[0] + k;
-            for (int i = 1; i < n; ++i) {
-                if (a[i] + k < min_a_plus_k) {
-                    min_a_plus_k = a[i] + k;
-                }
-            }
-            if (max_a > min_a_plus_k) {
-                cout << "0\n";
+            if (amax - amin > k) {
+                cout << 0;
             } else {
-                cout << (min_a_plus_k - max_a + 1) << '\n';
+                cout << k - (amax - amin) + 1;
             }
         }
+        cout << '\n';
     }
     return 0;
 }
