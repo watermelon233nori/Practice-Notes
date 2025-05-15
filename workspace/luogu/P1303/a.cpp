2694c80cbb694612a21756800e245599a269acc7
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2025-05-15 15:41:41 +0800

    Update Luogu P1303.

---
+++ workspace/luogu/P1303/a.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void times(string& a, string& b, string& c) {
+    
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string a, b, c;
+    a.reserve(2020);
+    b.reserve(2020);
+    cin>>a>>b;
+    c.reserve(4010);
+    return 0;
+}
