51965af1bd8acb2898f2dd56041c9fe84d4382b7
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2025-05-16 15:38:07 +0800

    Luogu P1009 Add. Luogu P1601 Update.

---
+++ workspace/luogu/P1009/a.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+char result[4000][4000];
+
+inline string times(string& a, string& b) {
+    auto [alen, blen] = pair<size_t, size_t>(a.length(), b.length());
+    string* str[2] = { &a, &b };
+    int startstr = 0;
+    if (alen > blen) {
+        startstr = 1;
+    }
+    vector<size_t> tmp;
+    auto maxlen = max(alen, blen);
+    auto minlen = min(alen, blen);
+    auto it_a = a.rbegin(), it_b = b.rbegin();
+    size_t carry = 0;
+    auto upper_rb = str[1 - startstr]->crbegin();
+    auto lower_rb = str[startstr]->crbegin();
+    for (int i = 0; i < minlen; ++i) {
+        auto upper_it = str[1 - startstr]->crbegin();
+        auto lower_it = str[startstr]->crbegin() + i;
+        for (int j = 0; j <= maxlen; ++j) {
+            auto num = (*upper_it ? *upper_it - '0' : 0) * (*lower_it ? *lower_it - '0' : 0) + carry;
+            carry = num / 10;
+            num %= 10;
+            result[i][j + i] = static_cast<uint8_t>(num);
+            upper_it++;
+        }
+    }
+    vector<int> vec(4050);
+    for (int i = 0; i < maxlen + minlen; ++i) {
+        int tmp = 0;
+        for (int j = 0; j < minlen; ++j) {
+            tmp += result[j][i];
+        }
+        tmp += carry;
+        carry = tmp / 10;
+        vec[i] = (tmp % 10);
+    }
+    auto complete = maxlen + minlen;
+    while (carry) {
+        int tmp = carry % 10;
+        carry %= 10;
+        vec[complete] = tmp;
+        complete++;
+    }
+    complete--;
+    string ret;
+    for (; vec[complete] == 0; complete--);
+    for (int i = complete; i >= 0; --i) {
+        ret.push_back(static_cast<string::value_type>(vec[i]) + '0');
+    } 
+    return ret;
+}
+
+void solve(int n) {
+    string res;
+    res.reserve(4000);
+    res = "1";
+    string sum;
+    for (int i = 1; i <= n; ++i) {
+        string now = to_string(i);
+        res = times(res, now);
+    }
+    for (const auto i: res) {
+        cout << i;
+    }
+    cout << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n; cin >> n;
+    solve(n);
+    return 0;
+}
---
+++ workspace/luogu/P1601/b.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string add(string& a, string& b) {
+    auto [alen, blen] = pair{a.length(), b.length()};
+    auto [maxlen, minlen] = pair{alen, blen};
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string a, b;
+    a.reserve(501);
+    b.reserve(501);
+    cin >> a >> b;
+    return 0;
+}
