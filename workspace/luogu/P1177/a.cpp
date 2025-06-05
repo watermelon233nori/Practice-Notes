7ee6367fa791593a5507fe40a91abe5b30f68060
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2025-06-05 13:52:10 +0800

    Update Luogu P1177 a.cpp.

--- workspace/luogu/P1177/a.cpp
+++ workspace/luogu/P1177/a.cpp
@@ -2,18 +2,43 @@
 
 using namespace std;
 
-inline void print_vector_no_space_at_last(vector<int>& num, bool lf = true) {
-    for (auto i = num.begin(); i != num.end() - 1; ++i) cout << *i << ' '; cout << num.back();
-    if (lf) cout << '\n';
+inline int medianThree(vector<int>& vec, int left, int middle, int right) {
+    const int l = vec[left], m = vec[middle], r = vec[right];
+    if (
+        (l >= m && l <= r) || (l >= r && l <= m)
+        ) return left;
+    if (
+        (m >= l && m <= r) || (m >= r && m <= l)
+        ) return middle;
+    return right;
 }
 
 int partition(vector<int>& num, int l, int r) {
-    
+    auto mid = (l + r) / 2;
+    auto median = medianThree(num, l, mid, r);
+    swap(num[median], num[l]);
+    int i = l, j = r;
+    while (i < j) {
+        while (i < j && num[j] >= num[l]) j--;
+        while (i < j && num[i] <= num[l]) i++;
+        swap(num[i], num[j]);
+    }
+    swap(num[l], num[i]);
+    return i;
 }
 
 void quick_sort(vector<int>& num, int l, int r) {
     auto len = num.size();
-    
+    while (l < r) {
+        auto pivot = partition(num, l, r);
+        if (pivot - l < r - pivot) {
+            quick_sort(num, l, pivot - 1);
+            l = pivot + 1;
+        } else {
+            quick_sort(num, pivot + 1, r);
+            r = pivot - 1;
+        }
+    }
 }
 
 int main() {
@@ -22,7 +47,6 @@ int main() {
     int n; cin >> n;
     vector<int> vec(n);
     for (size_t i = 0;i < n;++i)cin >> vec[i];
-    quick_sort(vec);
-    print_vector_no_space_at_last(vec);
+    quick_sort(vec, 0, vec.size() - 1);
     return 0;
 }
