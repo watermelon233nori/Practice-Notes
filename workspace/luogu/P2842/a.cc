11c058766ee8b60b07176dc3c06d15172158baab
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2025-10-24 15:35:37 +0800

    luogu p2842 add.

---
+++ workspace/luogu/P2842/a.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ull = unsigned long long;
+using ll = long long;
+
+vector<ll> v;
+
+ll backtrack(vector<ll>& types,ll balance, ll n, vector<ll>& sol) {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    v.reserve(1e3);
+    ll n, w;
+    cin >> n >> w;
+    for (ll i = 0; i < n; i++) {
+        ll tmp;
+        cin >> tmp;
+        v.emplace_back(tmp);
+    }
+    sort(v.begin(), v.end());
+
+    
+}
