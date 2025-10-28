a86b4ba7a0da045731a681aa7ef1fe2a0e18e6ed
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2025-10-28 16:15:59 +0800

    update luogu p1923.

--- workspace/luogu/P1923/243694283-AC.cpp
+++ workspace/luogu/P1923/243694283-AC.cpp
@@ -7,19 +7,6 @@ using ull = unsigned long long;
 ll n, k;
 vector<ll> v;
 
-void search_nth(ll l, ll r) {
-    if (l > r) return;
-    auto i = l, j = r;
-    while (i < j) {
-        while (i < j and v[j] >= v[l]) j--;
-        while (i < j and v[i] <= v[l]) i++;
-        swap(v[i], v[j]);
-    }
-    swap(v[i], v[l]);
-    search_nth(l, i);
-    search_nth(j, r);
-}
-
 int main() {
     ios::sync_with_stdio(0), cin.tie(0);
     cin >> n >> k;
@@ -29,4 +16,6 @@ int main() {
         cin >> tmp;
         v.emplace_back(tmp);
     }
+    sort(v.begin(), v.end());
+    cout << v[k] << '\n';
 }
