c1975230d761a50dcef1839cb191c94c1f0ddfb1
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2025-12-11 02:11:49 +0800

    luogu P2807 update.

---
+++ workspace/luogu/P2807/a.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        if (n == 1) {
+            cout << "1\n";
+            continue;
+        }
+    }
+}
