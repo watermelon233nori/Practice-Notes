3cb568fface91282dcab9e8f8986fdaee91121cc
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2026-02-09 01:51:22 +0800

    atcoder abc444 add, A AC.

---
+++ workspace/atcoder/abc444/A/AC.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    char c = getchar();
+    if (getchar() != c || getchar() != c) {
+        puts("No");
+    } else {
+        puts("Yes");
+    }
+}
