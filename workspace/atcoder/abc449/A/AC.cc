d44d7d83b5aa3b5368faf622b4765e9226a43024
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2026-03-16 17:39:58 +0800

    atcoder contest abc449 add, A AC

---
+++ workspace/atcoder/abc449/A/AC.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    long double d;
+    cin >> d;
+    cout << fixed << setprecision(16) << ((d/2) * (d/2) * M_PI) << '\n';
+}
