3573bb7cc67465243044d3bdcf2d3ae227b47c89
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2026-03-31 11:39:12 +0800

    update

--- workspace/nowcoder/contests/120561-2026WinterCamp1/G-upsolve/a.cc
+++ workspace/nowcoder/contests/120561-2026WinterCamp1/G-upsolve/a.cc
@@ -4,33 +4,48 @@ using namespace std;
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0);
-    int t;
-    cin >> t;
-    while (t--) {
+    int __T;
+    cin >> __T;
+    while (__T--) {
         string l, r;
         cin >> l >> r;
-        if (l == r) {
-            reverse(r.begin(), r.end());
-            cout << stoll(r) << '\n';
+        string t(1, '1');
+        int n = r.size();
+        for (int i = 0; i < n - 1; i++) {
+            t += '0';
+        }
+
+        if (r == t) {
+            if (l == r) {
+                cout << "1\n";
+            } else {
+                auto x = stoll(r);
+                cout << x - 1 << '\n';
+            }
             continue;
         }
 
-        if (r.front() == '1') {
-            auto siz = r.size();
-            bool flag = true;
-            for (int i = 1; i < siz; i++) {
-                if (r[i] != '0') {
-                    flag = false;
-                    break;
-                }
+        if (l.size() < r.size()) {
+            l = t;
+            l.back() += 1;
+            assert(l.size() == r.size());
+        }
+
+        int k = -1;
+        for (int i = 0; i < n; i++) {
+            if (l[i] != r[i]) {
+                k = i;
+                break;
             }
-            if (flag) {
-                siz--;
-                for (int i = 0; i < siz; i++) {
-                    cout << '9';
-                }
-                cout << '\n';
-                continue;
+        }
+
+        if (k == -1) {
+            string_view sv = r;
+            auto head = sv.find_first_not_of('0');
+            if (head == string::npos) {
+
+            } else {
+                
             }
         }
     }
