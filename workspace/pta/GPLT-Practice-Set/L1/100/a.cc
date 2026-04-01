38f2237cb06203ef33ca994a4b8e3e0f85009012
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2026-04-02 03:27:29 +0800

    pta gplt L1-100 add.

---
+++ workspace/pta/GPLT-Practice-Set/L1/100/a.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    int n, m;
+    cin >> n >> m;
+    ll sum{};
+    for (int i = 0; i < m; i++) {
+        ll a;
+        cin >> a;
+        sum += a;
+    }
+    
+}
