aa9262521c8b83c8ef3cc8b169f65e19d68b2a7d
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2026-04-14 15:20:18 +0800

    nowcoder tracker skill-tree update.

--- workspace/nowcoder/tracker/skill-tree/ch2/prefix-sum-and-difference/【模板】前缀和/AC.cc
+++ workspace/nowcoder/tracker/skill-tree/ch2/prefix-sum-and-difference/【模板】前缀和/AC.cc
@@ -1,20 +1,25 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
 
 int main() {
     ios::sync_with_stdio(0), cin.tie(0);
     int n, m;
     cin >> n >> m;
-    vector<int> v(n);
+    vector<ll> v(n);
     for (int i = 0; i < n; i++) {
-        int tmp;
+        ll tmp;
         cin >> tmp;
-        v[i] = v[min(0, i - 1)] + tmp;
+        v[i] = v[max(0, i - 1)] + tmp;
     }
     while (m--) {
-        int l,r;
+        int l, r;
         cin >> l >> r;
-        
+        ll sum = v[r - 1];
+        if (l > 1) {
+            sum -= v[l - 2];
+        }
+        cout << sum << '\n';
     }
 }
