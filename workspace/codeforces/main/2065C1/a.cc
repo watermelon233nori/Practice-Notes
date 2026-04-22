1c094ec7b2e4bb82d9a90b6284cff424da146870
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2026-04-22 14:07:29 +0800

    codeforces 2065C1 update.

--- workspace/codeforces/main/2065C1/a.cc
+++ workspace/codeforces/main/2065C1/a.cc
@@ -10,6 +10,15 @@ int main() {
     while (t--) {
         ll n, m;
         cin >> n >> m;
-        
+        vector<ll> va(n);
+        for (auto& i : va) cin >> i;
+        cin >> m;
+
+        ll idx = 1;
+        for (idx = 1; idx < va.size(); idx++) {
+            if (va[idx] < va[idx-1]) {
+                break;
+            }
+        }
     }
 }
