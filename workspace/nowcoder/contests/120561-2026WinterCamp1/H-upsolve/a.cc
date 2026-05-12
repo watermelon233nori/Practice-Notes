7759012f82d204486d26122fe27c6685fe897ed0
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2026-05-12 16:54:59 +0800

    nowcoder 120561H upsolve add.

---
+++ workspace/nowcoder/contests/120561-2026WinterCamp1/H-upsolve/a.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+using ull = unsigned ll;
+
+static inline void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
