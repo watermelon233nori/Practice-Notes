02353929c3df96391a8959d7e1994ba232d82c1e
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2026-05-15 01:32:21 +0800

    update nowcoder 120561H

--- workspace/nowcoder/contests/120561-2026WinterCamp1/H-upsolve/a.cc
+++ workspace/nowcoder/contests/120561-2026WinterCamp1/H-upsolve/a.cc
@@ -1,16 +1,32 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 using ll = long long;
-using ull = unsigned ll;
-
+using ull = unsigned long long;
+constexpr ll MOD = 998244353;
 static inline void solve() {
-    
-}
+    ll n;
+    cin >> n;
+    vector<ll> v(n + 1), dp(n + 1), pref(n + 1);
+    dp[0] = pref[0] = 1;
+    ll orval = 0;
+    ll wndl = 1;
+    // for (auto& i : v) cin >> i;
+    for (int i = 1; i <= n; i++) cin >> v[i]; // read
 
+    for (ll i = 1; i <= n; i++) {
+        while ((orval & v[i]) != 0) {
+            orval ^= v[wndl];
+            ++wndl;
+        }
+        orval |= v[i];
+    }
+}
 int main() {
+#ifdef ONLINE_JUDGE
     ios::sync_with_stdio(0), cin.tie(0);
-    int t;
+#endif
+    0;
+    ll t = 1;
     cin >> t;
     while (t--) {
         solve();
