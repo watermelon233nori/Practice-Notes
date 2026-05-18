fa655304783b4ab720f9deebdb1981f0fcbb00a6
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2026-05-19 03:27:38 +0800

    codeforces 2230A add.

---
+++ workspace/codeforces/main/2230A/a.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;using ll=long long;using ull=unsigned long long;constexpr ll MOD = 998244353;
+static inline void solve() {
+    
+}int main() {
+#ifdef ONLINE_JUDGE
+ios::sync_with_stdio(0),cin.tie(0);
+#elif 0
+freopen("1.in","rb",stdin);
+#endif
+0;
+ll t=1;cin>>t;while(t--){solve();}}
