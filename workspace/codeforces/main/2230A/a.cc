ed0e8e6965a8b2790fc813c54fe170c5975d68a2
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2026-05-19 16:00:45 +0800

    update

--- workspace/codeforces/main/2230A/a.cc
+++ workspace/codeforces/main/2230A/a.cc
@@ -1,12 +1,33 @@
 #include <bits/stdc++.h>
-using namespace std;using ll=long long;using ull=unsigned long long;constexpr ll MOD = 998244353;
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+constexpr ll MOD = 998244353;
 static inline void solve() {
-    
-}int main() {
-#ifdef ONLINE_JUDGE
-ios::sync_with_stdio(0),cin.tie(0);
+    ll n, a, b;
+    cin >> n >> a >> b;
+    ll ans = 0;
+    if (b / 3 > a) {
+        cout << a * n << '\n';
+    } else {
+        ll threecnt = n / 3;
+        if (threecnt > 0) {
+            ans += threecnt * b;
+            n %= 3;
+            
+        }
+    }
+}
+int main() {
+#if !(defined(ONLINE_JUDGE)) && 1
+    ios::sync_with_stdio(0), cin.tie(0);
 #elif 0
-freopen("1.in","rb",stdin);
+    freopen("1.in", "rb", stdin);
 #endif
-0;
-ll t=1;cin>>t;while(t--){solve();}}
+    0;
+    ll t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
