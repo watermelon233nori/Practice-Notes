5c2907b8b21bd9aacefe84dfb395c779e04c15eb
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2026-05-20 18:21:00 +0800

    codeforces 2230A AC

--- workspace/codeforces/main/2230A/AC.cc
+++ workspace/codeforces/main/2230A/AC.cc
@@ -7,20 +7,24 @@ static inline void solve() {
     ll n, a, b;
     cin >> n >> a >> b;
     ll ans = 0;
-    if (b / 3 > a) {
+    if (b / 3 >= a) {
         cout << a * n << '\n';
     } else {
         ll threecnt = n / 3;
         if (threecnt > 0) {
             ans += threecnt * b;
             n %= 3;
+            if (n) {
+                ans += min(b, a * n);
+            }
+            cout << ans << '\n';
         } else {
             cout << min(a * n, b) << '\n';
         }
     }
 }
 int main() {
-#if !(defined(ONLINE_JUDGE)) && 1
+#if defined(ONLINE_JUDGE) || 1
     ios::sync_with_stdio(0), cin.tie(0);
 #elif 0
     freopen("1.in", "rb", stdin);
