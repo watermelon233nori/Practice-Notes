5fa610e06523ef3b5b5cfe901212724bae38aac0
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2026-05-21 02:00:10 +0800

    update

---
+++ workspace/qoj/problems/ccpc/11/2661-zhengzhou/M-15313/a.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;using ll=long long;using ull=unsigned long long;constexpr ll MOD = 998244353;
+static inline void solve(){
+    string s;
+    cin>>s;
+    
+}int main(){
+#if defined(ONLINE_JUDGE)||1
+ios::sync_with_stdio(0),cin.tie(0);
+#elif 10
+freopen("1.in","rb",stdin);
+#endif
+ll t=1;cin>>t;while(t--){solve();}}
---
+++ workspace/qoj/problems/icpc/50/2581-nanjing/K-14811/a.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;using ll=long long;using ull=unsigned long long;constexpr ll MOD = 998244353;
+int grid[11][11];
+static inline void solve(){
+    int xn,yn,xr,yr;
+    cin>>xn>>yn>>xr>>yr;
+
+}int main(){
+#if defined(ONLINE_JUDGE)||1
+ios::sync_with_stdio(0),cin.tie(0);
+#elif 10
+freopen("1.in","rb",stdin);
+#endif
+ll t=1;cin>>t;while(t--){solve();}}
