7573dbdb699c0427c6ebbbf6052afed5909774c8
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2026-05-21 00:24:31 +0800

    update

---
+++ workspace/qoj/problems/icpc/50/2581-nanjing/C-14803/AC.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+constexpr ll MOD = 998244353;
+static inline void solve() {
+    ll n;
+    cin >> n;
+    if (n % 2) cout << "No\n"sv;
+    else cout << "Yes\n"sv << n / 2 << ' ' << n / 2 << '\n';
+}
+int main() {
+#if defined(ONLINE_JUDGE) || 1
+    ios::sync_with_stdio(0), cin.tie(0);
+#elif 10
+    freopen("1.in", "rb", stdin);
+#endif
+    ll t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
