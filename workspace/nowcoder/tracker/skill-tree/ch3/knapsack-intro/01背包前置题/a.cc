91e4ef91e1691c5c991fbc7821e118231da72c42
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2026-05-22 21:33:45 +0800

    nowcoder tracker skill-tree add chapter 3

---
+++ workspace/nowcoder/tracker/skill-tree/ch3/knapsack-intro/01背包前置题/a.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;using ll=long long;using ull=unsigned long long;constexpr ll MOD = 998244353;
+static inline void solve(){
+    int n,k;
+}int main(){
+#if defined(ONLINE_JUDGE)||1
+ios::sync_with_stdio(0),cin.tie(0);
+#elif 10
+freopen("1.in","rb",stdin);
+#endif
+ll t=1;;while(t--){solve();}}
