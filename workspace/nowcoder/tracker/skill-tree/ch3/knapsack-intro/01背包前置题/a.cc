7864ff36cd6c1e9693a9bfbbd6c4c43736aced8d
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2026-05-23 11:30:32 +0800

    update

--- workspace/nowcoder/tracker/skill-tree/ch3/knapsack-intro/01背包前置题/a.cc
+++ workspace/nowcoder/tracker/skill-tree/ch3/knapsack-intro/01背包前置题/a.cc
@@ -1,11 +1,23 @@
 #include <bits/stdc++.h>
-using namespace std;using ll=long long;using ull=unsigned long long;constexpr ll MOD = 998244353;
-static inline void solve(){
-    int n,k;
-}int main(){
-#if defined(ONLINE_JUDGE)||1
-ios::sync_with_stdio(0),cin.tie(0);
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+constexpr ll MOD = 998244353;
+static inline void solve() {
+    int n, k;
+    vector<int> v(n);
+    vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
+    
+}
+int main() {
+#if defined(ONLINE_JUDGE) || 1
+    ios::sync_with_stdio(0), cin.tie(0);
 #elif 10
-freopen("1.in","rb",stdin);
+    freopen("1.in", "rb", stdin);
 #endif
-ll t=1;;while(t--){solve();}}
+    ll t = 1;
+    ;
+    while (t--) {
+        solve();
+    }
+}
