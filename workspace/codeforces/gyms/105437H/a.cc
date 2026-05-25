00eca44aad8de8f75ead92a63efd2934b22d234a
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2026-05-25 17:44:09 +0800

    codeforces gym 105437H add

---
+++ workspace/codeforces/gyms/105437H/a.cc
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define mypow(x) ((x) * (x))
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+constexpr ll MOD = 998244353;
+static inline void solve() {
+    struct seg {
+        char c;
+        ll len;
+    };
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    vector<seg> segments;
+    for (int i = 0; i < n;) {
+        int j = i;
+        while (j < n && s[j] == s[i]) j++;
+        auto len = j - i;
+        segments.emplace_back(s[i], len);
+        i = j;
+    }
+    // for (auto [c,len]: segments) {
+    //     cout << c << ' ' << len << '\n';
+    // }
+    int seg_cnt = segments.size();
+    ll maxdiff = LLONG_MIN;
+    for (int i = 0; i < seg_cnt - 1; i++) {
+        // swap(seg1,seg2)
+
+        // if both seg's len equal to 1, consider diff as 1
+        if (segments[i].len == 1 && segments[i + 1].len == 1) {
+            maxdiff = max(maxdiff, (ll)1);
+        }
+
+// calculate original
+        ll original_sum = mypow(segments[i].len) + mypow(segments[i + 1].len);
+
+        // ll arr[2] = {segments[i].len - 1, segments[i + 1].len - 1};
+        //         ll arr[2] = {mypow(segments[i].len) - mypow(segments[i].len - 1),
+        //                      mypow(segments[i + 1].len) - mypow(segments[i + 1].len - 1)};
+        // ll diff = segments[i].len == 1 || segments[i+1].len == 1;
+        ll new_sum = mypow(segments[i].len - 1) + mypow(segments[i + 1].len);
+        if (i != 0) {
+
+        }
+
+        // maxdiff = max(maxdiff, diff);
+    }
+
+    ll beauty{};
+    for (auto [_, len] : segments) {
+        beauty += len * len;
+    }
+    cout << beauty + maxdiff << '\n';
+}
+int main() {
+#if defined(ONLINE_JUDGE) || 1
+    ios::sync_with_stdio(0), cin.tie(0);
+#elif 10
+    freopen("1.in", "rb", stdin);
+#endif
+    ll t = 1;
+    while (t--) {
+        solve();
+    }
+}
