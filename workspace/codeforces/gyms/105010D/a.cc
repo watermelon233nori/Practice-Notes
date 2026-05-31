3521537ab1d964179fc7edb26b31da78e839a423
Westmelon233 <92632254+watermelon233nori@users.noreply.github.com>
2026-06-01 01:00:00 +0800

    codeforces gyms105010D add

---
+++ workspace/codeforces/gyms/105010D/a.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+constexpr ll MOD = 998244353;
+static inline void solve() {
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> v(n);
+    for (auto& i : v) cin >> i;
+
+}
+int main() {
+#if defined(ONLINE_JUDGE) || 1
+    ios::sync_with_stdio(0), cin.tie(0);
+#elif 10
+    freopen("1.in", "rb", stdin);
+#endif
+    ll t = 1;
+    while (t--) {
+        solve();
+    }
+}
